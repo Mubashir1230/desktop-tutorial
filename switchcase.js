d57379310e10f517045bfa4127e747622c3b13c4
Mubashir1230 <116015084+Mubashir1230@users.noreply.github.com>
2022-11-23 20:56:11 +0500

    Add files via upload

---
+++ switchcase.js
@@ -0,0 +1,73 @@
+// let x = prompt("Any Number from 1 to 5");
+// let y = parseInt(x);
+// switch(x){
+// case(x==5):
+// document.getElementById("abc").innerHTML="This is first case" +"<br>" + x;
+// break;
+// case(x>5):
+// document.getElementById("abc").innerHTML="This is Second case" +"<br>" + x;
+// break;
+// case(x<5):
+// document.getElementById("abc").innerHTML="This is Third case" +"<br>" + x;
+// break;
+// default:
+//     document.getElementById("abc").innerHTML="Wrong case" +"<br>" + x;
+// break;
+// }
+
+
+
+
+
+// write a number which shows month
+// var x = 1;
+// console.log(x);
+// switch(true){
+//     case ((x == "january") || (x == 1)):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "February" || x == 2):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "march" || x == 3):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "April" || x == 4):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "may" || x == 5):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "June" || x == 6):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "july" || x == 7):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "august" || x == 8):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "september" || x == 9):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "octuber" || x == 10):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "November" || x == 11):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     case(x == "december" || x == 12):
+//     document.getElementById("abc").innerHTML= "This is the first month" + x;
+//     break;
+//     default:
+//         document.getElementById("abc").innerHTML= "Not a Number" + x;
+//     break;
+// }
+
+
+// write a program to print any table by using for loop
+var x = prompt("Enter Any Number");
+var x = parseInt(x);
+for(var y = 1;y<=10;y++){
+    console.log(x * y);
+}
