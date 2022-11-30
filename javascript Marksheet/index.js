9d4b68b6920653b2974bf8e35256bb6686960667
Mubashir1230 <116015084+Mubashir1230@users.noreply.github.com>
2022-11-30 08:01:32 -0800

    Add files via upload

---
+++ javascript Marksheet/index.js
@@ -0,0 +1,6 @@
+import {a,b,c} from "./module/fileone.js";
+document.getElementById("Percentage").innerHTML= a+b+c;
+document.getElementById("english").innerHTML= a;
+document.getElementById("Urdu").innerHTML= b;
+document.getElementById("Maths").innerHTML= c;
+
---
+++ javascript Marksheet/module/fileone.js
@@ -0,0 +1,8 @@
+   var a = prompt("Enter English Marks");
+   export var a = parseInt(a);
+    var b = prompt("Enter Urdu Marks");
+    export var b = parseInt(b);
+    var c = prompt("Enter Maths Marks");
+    export var c = parseInt(c);
+
+    
