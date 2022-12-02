5f121ed620a6cfb06c70925bd9f44ff24b27eff4
Mubashir1230 <116015084+Mubashir1230@users.noreply.github.com>
2022-12-02 20:57:15 +0500

    Add files via upload

--- javascript Marksheet/index.js
+++ javascript Marksheet/index.js
@@ -1,6 +1,23 @@
-import {a,b,c} from "./module/fileone.js";
-document.getElementById("Percentage").innerHTML= a+b+c;
+import {a,b,c,d,e,name} from "./module/fileone.js";
+document.getElementById("Total").innerHTML= d;
 document.getElementById("english").innerHTML= a;
 document.getElementById("Urdu").innerHTML= b;
 document.getElementById("Maths").innerHTML= c;
+document.getElementById("Percentage").innerHTML= e+"%";
+document.getElementById("Name").innerHTML= name;
 
+if(e<=100 && e>=80){
+    document.getElementById("Grade").innerHTML= "A+";
+}
+else if(e<=80 && e>=70){
+    document.getElementById("Grade").innerHTML= "A";
+}
+else if(e<=70 && e>=60){
+    document.getElementById("Grade").innerHTML= "B";
+}
+else if(e<=60 && e>=50){
+    document.getElementById("Grade").innerHTML= "C";
+}
+else{
+    document.getElementById("Grade").innerHTML= "Failed";
+}
--- javascript Marksheet/module/fileone.js
+++ javascript Marksheet/module/fileone.js
@@ -1,3 +1,4 @@
+    export var name = prompt("Your Name");
    var a = prompt("Enter English Marks");
    export var a = parseInt(a);
     var b = prompt("Enter Urdu Marks");
@@ -5,4 +6,5 @@
     var c = prompt("Enter Maths Marks");
     export var c = parseInt(c);
 
-    
+    export var d = a+b+c;
+    export var e = ((d/300) * 100);
